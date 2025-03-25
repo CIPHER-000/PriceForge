c1bc3b452e6e2a0d54d75230ec9e0a5a6bdf9c93
Henry Giwa <favourgiwa3@gmail.com>
2025-03-25 12:40:24 +0100

    removed uncesarry files

--- app/utils/web3provider.js
+++
@@ -1,7 +0,0 @@
-const { ethers } = require("ethers");
-const { INFURA_API_KEY } = require("../config/dotenv");
-
-// Initialize provider
-const provider = new ethers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`);
-
-module.exports = provider;
